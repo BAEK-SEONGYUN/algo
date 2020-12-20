f6dce84c9b592e34eddbaf468070ccea863744e0
BAEK-SEUNGYUN <bsy6953@gmail.com>
2020-12-20 22:08:15 +0900

    commit

---
+++ Main2231.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main2231 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException{
+        
+        int n = Integer.parseInt(br.readLine());
+
+        int len = String.valueOf(n).length();
+
+        int sol = 0;
+
+        for (int i = n-(9*len); i <= n; i++) {
+            
+        }
+
+    }
+}
---
+++ Main2798.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main2798 {
+    
+
+    public static void main(String[] args) throws IOException{
+        
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int sol = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < arr.length-2; i++) {
+            if(arr[i] > m){
+                continue;
+            }
+            for (int j = i+1; j < arr.length-1; j++) {
+                if(arr[i] + arr[j] > m){
+                    continue;
+                }
+                for (int k = j+1; k < arr.length; k++) {
+                    int temp = arr[i] + arr[j] + arr[k];
+                    if(m == temp){
+                        sol = temp;
+                    }
+
+                    if(sol < temp && temp < m){
+                        sol = temp;
+                    }
+                }
+            }
+        }
+        System.out.println(sol);
+    }
+}
